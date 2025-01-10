735c71f5dbd07a93f79a33067c123ae4699b9425
Zomwolff <aman4m4@gmail.com>
2025-01-10 10:04:39 +0530

    more Ui changes
    added avatar

--- UI.gd
+++ UI.gd
@@ -6,6 +6,7 @@ extends CanvasLayer
 @onready var inv_screen = get_parent().get_child(3)
 @onready var shop_screen = get_parent().get_child(4)
 @onready var com_screen = $Panel
+@onready var av_screen = $Panel2
 
 
 #buttons
@@ -25,6 +26,7 @@ var shop = false
 var home = true
 var inventory = false
 var com = false
+var avt = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -36,45 +38,69 @@ func _process(delta):
 		inv_screen.visible = false
 		shop_screen.visible = false
 		com_screen.visible = false
+		av_screen.visible = false
 		shop_texture.texture = shop_icon1
 		
 	elif shop == true:
 		inv_screen.visible = false
 		shop_screen.visible = true
 		com_screen.visible = false
+		av_screen.visible = false
 		shop_texture.texture = shop_icon2
 
 	elif inventory == true:
 		inv_screen.visible = true
 		shop_screen.visible = false
 		com_screen.visible = false
+		av_screen.visible = false
 		shop_texture.texture = shop_icon1
 
 	elif com == true:
-				com_screen.visible = true
-				shop_screen.visible = false
-				inv_screen.visible = false
+		com_screen.visible = true
+		shop_screen.visible = false
+		inv_screen.visible = false
+		av_screen.visible = false
+	
+	elif avt == true:
+		com_screen.visible = false
+		shop_screen.visible = false
+		inv_screen.visible = false
+		av_screen.visible = true
 
 func _on_shop_released():
 	shop = true
 	home = false
 	inventory = false
 	com = false
+	avt = false
 
 func _on_home_released():
 	shop = false
 	home = true
 	inventory = false
 	com = false
+	avt = false
 
 func _on_inventory_released():
 	shop = false
 	home = false
 	inventory = true
 	com = false
+	avt = false
 
 func _on_community_released():
 	shop = false
 	home = false
 	inventory = false
 	com = true
+	avt = false
+
+
+
+
+func _on_touch_screen_button_released():
+	shop = false
+	home = false
+	inventory = false
+	com = false
+	avt = true
